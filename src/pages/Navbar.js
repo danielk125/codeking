bceb42bb043f8aef1818ae4b16f9a068bb60de83
danielk125 <158085907+danielk125@users.noreply.github.com>
2024-11-07 00:48:56 -0600

    hw5 submission

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,6 +1,13 @@
 import logo from '../CodeKingTrans.png';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+    const navigate = useNavigate();
+
+    const login = () => {
+        navigate('/');
+      }
+
     return(
         <div className='navbar'>
             <img alt='Code King Logo' src={logo}></img>
@@ -8,7 +15,7 @@ function Navbar() {
             CODE KING
             </h1>
             <div className='nav_buttons'>
-            <button>
+            <button onClick={login}>
                 LOGIN
             </button>
             <button>
